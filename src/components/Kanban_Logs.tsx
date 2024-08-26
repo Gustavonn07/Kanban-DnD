b22a9974b518f5a811bac9d09b597a6bc5e72e44
Gustavonn07 <gustavonepnog@gmail.com>
2024-08-26 16:43:49 -0300

    refact: refatorando codigo, adicionando componente de modal

--- src/components/Kanban_Logs.tsx
+++ src/components/Kanban_Logs.tsx
@@ -1,7 +1,7 @@
 import { twMerge } from "tailwind-merge";
 import { Column, Log } from "../types";
-import Close_Icon from "./icons/Close_Icon";
 import { truncateString } from "../utils/truncateString";
+import Kanban_Modal from "./Kanban_Modal";
 
 interface Props {
     setOpenLogModal: (openLogModal: boolean) => void;
@@ -81,23 +81,10 @@ function Log_Container({ log, columns, index }: PropsContainer) {
 function Kanban_Logs({ setOpenLogModal, logs, columns }: Props) {
 
   return (
-
-    <section 
-        onClick={() => setOpenLogModal(false)}
-        className="absolute left-0 w-full h-screen flex justify-center items-center bg-[#00000080]"
+    <Kanban_Modal
+        setOpenModal={setOpenLogModal}
     >
-        <div
-            onClick={(e) => e.stopPropagation()}
-            className="w-10/12 relative stroke-2 h-[90vh] rounded shadow-md shadow-[#00000060] bg-mainBackgroundColor"
-        >
-            <button 
-                onClick={() => setOpenLogModal(false)}
-                className="absolute top-4 right-6 hover:bg-rose-500 rounded cursor-pointer duration-150"
-            >
-                <Close_Icon/>
-            </button>
-
-            <ul className="flex flex-col gap-6 overflow-y-auto h-full w-2/3 p-10">
+        <ul className="flex flex-col gap-6 overflow-y-auto h-full w-2/3 p-10">
                 {
                     logs.map((log, index) => (
                         <Log_Container 
@@ -108,9 +95,7 @@ function Kanban_Logs({ setOpenLogModal, logs, columns }: Props) {
                     ))
                 }
             </ul>
-
-        </div>
-    </section>
+    </Kanban_Modal>
   );
 }
 
---
+++ src/components/Kanban_Modal.tsx
@@ -0,0 +1,32 @@
+import Close_Icon from "./icons/Close_Icon";
+import { ReactNode } from "react";
+
+interface Props {
+    setOpenModal: (openGraphicsModal: boolean) => void;
+    children: ReactNode
+}
+
+function Kanban_Modal({ setOpenModal, children }: Props) {
+
+    return (
+        <section 
+            onClick={() => setOpenModal(false)}
+            className="absolute left-0 w-full h-screen flex justify-center items-center bg-[#00000080]"
+        >
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className="w-10/12 relative stroke-2 h-[90vh] rounded shadow-md shadow-[#00000060] bg-mainBackgroundColor"
+            >
+                <button 
+                    onClick={() => setOpenModal(false)}
+                    className="absolute top-4 right-6 hover:bg-rose-500 rounded cursor-pointer duration-150"
+                >
+                    <Close_Icon/>
+                </button>
+                {children}
+            </div>
+        </section>
+    )
+}
+
+export default Kanban_Modal;
